cb7104a310710f93ecc5ea28313b353e4d910b11
Jack Jutzi <jsjutzi@gmail.com>
2020-12-17 20:39:43 -0600

    define main function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use ferris_says::say;
+use std::io::{stdout, BufWriter, BufReader};
+
 fn main() {
-    println!("Hello, world!");
+    let stdout = stdout();
+    let message = String::from("I'm in danger :)");
+    let width = message.chars().count();
+
+    let mut writer = BufWriter::new(stdout.lock());
+    say(message.as_bytes(), width, &mut writer).unwrap();
 }
